06dc0ba8f976e429c2f86ce7caca29cf37ff1e42
jerzha40 <jerryzhang40@gmail.com>
2026-02-07 13:01:08 +0800

    all

--- proj/src/main.rs
+++ proj/src/main.rs
@@ -1,3 +1,19 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Tank Test Window".to_string(),
+                resolution: (1280.0, 720.0).into(),
+                ..default()
+            }),
+            ..default()
+        }))
+        .add_systems(Startup, setup_camera)
+        .run();
+}
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
 }
